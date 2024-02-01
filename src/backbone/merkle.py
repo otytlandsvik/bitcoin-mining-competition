0877a3fcaa6b03b70a9babc5587ad20cbbedc3aa
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2024-02-01 13:39:40 +0100

    accept hash input to merkle tree instead of tx abstraction

--- src/backbone/merkle.py
+++ src/backbone/merkle.py
@@ -1,16 +1,16 @@
+from copy import copy
 from utils.cryptographic import hash_function
 from abstractions.transaction import Transaction
 
 
 class Node:
-    def __init__(self, left, right, tx, _hash) -> None:
+    def __init__(self, left, right, _hash) -> None:
         self.left = left
         self.right = right
-        self.tx = tx
         self.hash = _hash
 
     def __str__(self) -> str:
-        if self.tx != None:
+        if self.hash != None:
             return f"[{self.tx} -> {self.hash}]"
         elif self.left != None and self.right != None:
             return f"[{self.left.hash} + {self.right.hash} -> {self.hash}]"
@@ -19,16 +19,16 @@ class Node:
 
 
 class MerkleTree:
-    def __init__(self, txs: list[Transaction]) -> None:
+    def __init__(self, txs: list[str]) -> None:
         self.data = None
         self.leaf_nodes: list[Node] = self.insert_leaves(txs)
         self.root = None
         self.build_tree()
 
-    def insert_leaves(self, txs: list[Transaction]) -> list[Node]:
+    def insert_leaves(self, txs: list[str]) -> list[Node]:
         nodes = []
-        for tx in txs:
-            nodes.append(Node(None, None, tx, tx.hash))
+        for _hash in txs:
+            nodes.append(Node(None, None, _hash))
         return nodes
 
     def build_tree(self) -> None:
@@ -37,19 +37,19 @@ class MerkleTree:
     def __rec_build_tree(self, nodes: list[Node]) -> Node:
         # Handle odd number of nodes
         if len(nodes) % 2 == 1:
-            nodes.append(nodes[-1].copy())
+            nodes.append(copy(nodes[-1]))
 
         if len(nodes) == 2:
             l = nodes[0]
             r = nodes[1]
-            return Node(l, r, None, hash_function(l.hash + r.hash))
+            return Node(l, r, hash_function(l.hash + r.hash))
 
         # Divide and conquer
         middle = len(nodes) // 2
         l = self.__rec_build_tree(nodes[:middle])
         r = self.__rec_build_tree(nodes[middle:])
 
-        return Node(l, r, None, hash_function(l.hash + r.hash))
+        return Node(l, r, hash_function(l.hash + r.hash))
 
     def get_root(self) -> Node:
         return self.root
